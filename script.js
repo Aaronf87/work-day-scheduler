011d6b18e157c311cc0e79aaf9772f134cda1b97
Aaron Flores <aaron_flores08@yahoo.com>
2023-07-06 23:22:30 -0500

    added comments

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-dayjs().day() // gets day of current week
-console.log(dayjs());
+// dayjs().day() // gets day of current week
+// console.log(dayjs());
 
 // $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. 
@@ -20,7 +21,16 @@ console.log(dayjs());
   
   // HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
+  // past, present, and future classes? 
+
+
+  console.log(past);
+
+  $("#past").css("color" ,"blue");
+  $.datepicker.formatDate('yy/mm/dd', new Date());
+
+  
+  // How can Day.js be used to get the
   // current hour in 24-hour time?
 
   // displayTime(){
