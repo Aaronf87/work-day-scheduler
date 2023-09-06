c87c8b4262a4131a64b0b1c5eda143d977e43ae6
Aaron Flores <aaron_flores08@yahoo.com>
2023-09-06 18:00:32 -0500

    created variables in jquery

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var myDate = new Date();
 
 var date = document.getElementById("currentDay")
 date.innerHTML = myDate;
+var myDate = new Date();
+var myDateString = myDate.toDateString();
 
 
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(document).ready(function () {
 
-
-// $(function () {
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  // local storage. 
+  // local storage.
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
+    localStorage.setItem(time, text);
+  });
   
   // HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the
 
   // time-block containing the button that was clicked? How might the id be
 
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour.
+  function timeTracker() {
+    //get current number of hours.
+    var timeNow = moment().hour();
+    
   
   // HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
